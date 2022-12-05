f1d7d8bac90a2ff5cff9fa22469db14729436a4c
Wendell Teske <wendelltteske@gmail.com>
2022-12-04 22:23:47 -0300

    Primeiro Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+Created by Wendell Thomas Teske.
+https://www.linkedin.com/in/wendelltteske/
---
+++ src/App.js
@@ -0,0 +1,9 @@
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="container">
+      <h2>Olá, Mundo! :D</h2>
+    </div>
+  );
+}
